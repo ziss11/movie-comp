50062948fb8ad1e66e5b058bf872299e2593efd4
Abdul Azis <abdul.azizz1108@gmail.com>
2022-12-02 11:25:46 +0700

    create repository interface

--- app/src/main/java/com/example/movieappcompose/domain/repositories/MovieRepository.kt
+++ app/src/main/java/com/example/movieappcompose/domain/repositories/MovieRepository.kt
@@ -1,4 +1,24 @@
 package com.example.movieappcompose.domain.repositories
 
+import androidx.lifecycle.LiveData
+import com.example.movieappcompose.data.models.MovieModel
+import com.example.movieappcompose.utilities.ResultState
+
 interface MovieRepository {
+    fun getPopularMovies(apiKey: String): LiveData<ResultState<List<MovieModel>>>
+    fun getNowPlayingMovies(apiKey: String): LiveData<ResultState<List<MovieModel>>>
+    fun getRecommendedMoviesById(
+        movieId: Int,
+        apiKey: String
+    ): LiveData<ResultState<List<MovieModel>>>
+
+    fun getMovieDetail(
+        movieId: Int,
+        apiKey: String
+    ): LiveData<ResultState<MovieModel>>
+
+    fun searchMovie(
+        apiKey: String,
+        query: String
+    ): LiveData<ResultState<List<MovieModel>>>
 }
